040fd1dda211682b0ea5e02425b8647adce1ffad
thsh0881 <thsh0881@gmail.com>
2026-09-02 11:17:01 -0400

    Add List Accounts endpoint and tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -64,6 +64,15 @@ class TestAccountService(TestCase):
         """It should not Create an Account when sending bad data"""
         resp = self.client.post(BASE_URL, json={"name": "not enough data"})
         self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+    
+    def test_get_account_list(self):
+        """It should Get a list of Accounts"""
+        self._create_accounts(5)
+        resp = self.client.get(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 5)
+        
     ######################################################################
     #  H E L P E R   M E T H O D S
     ######################################################################
